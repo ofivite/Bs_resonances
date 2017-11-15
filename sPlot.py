bc84bf0261a3374d9bef75914bd833ec90b35c03
Oleg Filatov <oleg.filatov@cern.ch>
2017-11-16 01:15:21 +0300

    phi 5 MeV with mass-window cut

--- sPlot.py
+++ sPlot.py
@@ -11,12 +11,12 @@ file_data = ROOT.TFile('new.root')
 file_MC_psi = ROOT.TFile('SimpleFileMC_b715psi_0_14000.root')
 file_MC_X = ROOT.TFile('SimpleFileMC_b715x_0_14000.root')
 
-cuts = 'TMath::Abs(PHI_mass_Cjp - 1.02)<0.01 && PIPI_mass_Cjp > X_mass_Cjp - 3.0969 - 0.15 && BU_mass_Cjp > ' + str(left_discr) + '&& BU_mass_Cjp < ' + str(right_discr)
-cuts_psi = 'X_mass_Cjp >' + str(left_psi) + ' && X_mass_Cjp < ' + str(right_psi) # + ' && PIPI_mass_Cjp > 0.4 && PIPI_mass_Cjp < 0.6'
-cuts_X = 'X_mass_Cjp >' + str(left_X) + ' && X_mass_Cjp < ' + str(right_X) # + ' && PIPI_mass_Cjp > 0.65 && PIPI_mass_Cjp < 0.78'
+cuts = 'TMath::Abs(PHI_mass_Cjp - 1.02)<0.005 && BU_mass_Cjp > ' + str(left_discr) + '&& BU_mass_Cjp < ' + str(right_discr) # + '&& PIPI_mass_Cjp > X_mass_Cjp - 3.0969 - 0.15'
+cuts_psi = 'X_mass_Cjp >' + str(left_psi) + ' && X_mass_Cjp < ' + str(right_psi)  + ' && PIPI_mass_Cjp > 0.4 && PIPI_mass_Cjp < 0.6'
+cuts_X = 'X_mass_Cjp >' + str(left_X) + ' && X_mass_Cjp < ' + str(right_X)  + ' && PIPI_mass_Cjp > 0.65 && PIPI_mass_Cjp < 0.78'
 
 c = ROOT.TCanvas("c", "c", 1700, 650)
-c.Divide(3,2)
+c.Divide(2,2)
 
 ####################################################
 #_-_-_-_-_-_-      INCLUSIVE DATA      _-_-_-_-_-_-#
@@ -49,7 +49,16 @@ print '\n\n' + 30*'#' + '\n\n\n           psi(2S) now\n\n\n' + 30*'#' + '\n\n'
 data_psi = data.reduce(cuts_psi)
 # data_psi = ROOT.RooDataSet('data', '', file_MC_psi.Get('mytree'), ROOT.RooArgSet(var_discr, var_control, PIPI_mass_Cjp, PHI_mass_Cjp), cuts + '&& ' + cuts_psi)
 
-sigma_Bs.setVal(0.006234); gamma_BW_Bs.setVal(0.007085)
+# # phi 10 Diag
+# sigma_Bs.setVal(0.006234); gamma_BW_Bs.setVal(0.007085)
+# sigma_Bs.setConstant(1);  gamma_BW_Bs.setConstant(1)
+
+# phi 10 MW
+# sigma_Bs.setVal(0.006120); gamma_BW_Bs.setVal(0.007315)
+# sigma_Bs.setConstant(1);  gamma_BW_Bs.setConstant(1)
+
+# phi 5 MW
+sigma_Bs.setVal(0.006232); gamma_BW_Bs.setVal(0.006851)
 sigma_Bs.setConstant(1);  gamma_BW_Bs.setConstant(1)
 
 # sigma_Bs_1.setVal(0.01923);  sigma_Bs_2.setVal(0.007333)
@@ -80,17 +89,19 @@ sData_psi = ROOT.RooStats.SPlot(
     'sData_psi', 'sData_psi', data_psi, model_discr,
     ROOT.RooArgList(N_sig_discr, N_bkgr_discr)
 )
-
-print '\n\n\n\n\n\n\n\n\n\n\n\n'
-print "Yield of signal is " , N_sig_discr.getVal() , ".  From sWeights it is " , sData_psi.GetYieldFromSWeight("N_sig_discr")
-print '\n\n\n\n\n\n\n\n\n\n\n\n'
+print '\n\n' + 30*'#' + '\n\n\n           psi sPlot now\n\n\n' + 30*'#' + '\n\n'
 
 
 c.cd(2)
 var_control.setMin(left_psi); var_control.setMax(right_psi)
 data_psi_weighted = ROOT.RooDataSet(data_psi.GetName(),data_psi.GetTitle(),data_psi, data_psi.get(), cuts + '&& ' + cuts_psi, "N_sig_discr_sw") ;
 
-sigma_psi.setVal(0.002804); gamma_BW_psi.setVal(0.003435)
+# phi 10 MW
+# sigma_psi.setVal(0.002639); gamma_BW_psi.setVal(0.003858)
+# sigma_psi.setConstant(1);  gamma_BW_psi.setConstant(1)
+
+# phi 5 MW
+sigma_psi.setVal(0.00268746); gamma_BW_psi.setVal(0.00372264)
 sigma_psi.setConstant(1);  gamma_BW_psi.setConstant(1)
 
 # sigma_psi_1.setVal(0.00668);  sigma_psi_2.setVal(0.003120)
@@ -124,7 +135,12 @@ print '\n\n' + 30*'#' + '\n\n\n           X now\n\n\n' + 30*'#' + '\n\n'
 data_X = data.reduce(cuts_X)
 # data_X = ROOT.RooDataSet('data', '', file_MC_X.Get('mytree'), ROOT.RooArgSet(var_discr, var_control, PIPI_mass_Cjp, PHI_mass_Cjp), cuts + '&& ' + cuts_X)
 
-sigma_Bs.setVal(0.00521); gamma_BW_Bs.setVal(0.00996)
+# phi 10 MW
+# sigma_Bs.setVal(0.00566); gamma_BW_Bs.setVal(0.00867)
+# sigma_Bs.setConstant(1);  gamma_BW_Bs.setConstant(1)
+
+# phi 5 MW
+sigma_Bs.setVal(0.0057); gamma_BW_Bs.setVal(0.00829)
 sigma_Bs.setConstant(1);  gamma_BW_Bs.setConstant(1)
 
 # sigma_Bs_1.setConstant(0); sigma_Bs_2.setConstant(0)
@@ -157,16 +173,23 @@ ROOT.RooStats.SPlot(
     ROOT.RooArgList(N_sig_discr,N_bkgr_discr)
 )
 
-c.cd(5)
+print '\n\n' + 30*'#' + '\n\n\n           X splot now\n\n\n' + 30*'#' + '\n\n'
 
+# c.cd(5)
+#
 var_control.setMin(left_X); var_control.setMax(right_X)
 data_X_weighted = ROOT.RooDataSet(data_X.GetName(),data_X.GetTitle(),data_X, data_X.get(), cuts + '&& ' + cuts_X, "N_sig_discr_sw") ;
 
-sigma_X.setVal(0.004039); gamma_BW_X.setVal(0.004800)
+# # phi 10 MW
+# sigma_X.setVal(0.004265); gamma_BW_X.setVal(0.004021)
+# sigma_X.setConstant(1);  gamma_BW_X.setConstant(1)
+
+# phi 10 MW
+sigma_X.setVal(0.0043164); gamma_BW_X.setVal(0.0038798)
 sigma_X.setConstant(1);  gamma_BW_X.setConstant(1)
 
-# sigma_X_1.setVal(0.005049);  sigma_X_2.setVal(0.01456); fr_X.setVal(0.775);
-# sigma_X_1.setConstant(1); sigma_X_2.setConstant(1); fr_X.setConstant(1);
+# # sigma_X_1.setVal(0.005049);  sigma_X_2.setVal(0.01456); fr_X.setVal(0.775);
+# # sigma_X_1.setConstant(1); sigma_X_2.setConstant(1); fr_X.setConstant(1);
 #
 N_sig_X.setVal(0)
 N_sig_X.setConstant(1)
@@ -175,13 +198,13 @@ N_sig_X.setConstant(1)
 rrr_null = model_X.fitTo(data_X_weighted, RF.Save(), RF.SumW2Error(ROOT.kFALSE))
 # rrr_null = model_X.fitTo(data_X_weighted, RF.Save(), RF.SumW2Error(ROOT.kFALSE))
 # rrr_null = model_X.fitTo(data_X_weighted, RF.Save(), RF.SumW2Error(ROOT.kFALSE))
-#
-frame_control = ROOT.RooPlot(" ", 'm(J/#psi#pi^{+}#pi^{-}#phi) bkgr only', var_control, left_X, right_X, nbins_X);
-data_X_weighted.plotOn(frame_control)
-model_X.paramOn(frame_control, RF.Layout(0.55, 0.87, 0.6))
-model_X.plotOn(frame_control, RF.LineColor(ROOT.kRed-6), RF.LineWidth(5))
 
-frame_control.Draw()
+# frame_control = ROOT.RooPlot(" ", 'm(J/#psi#pi^{+}#pi^{-}#phi) bkgr only', var_control, left_X, right_X, nbins_X);
+# data_X_weighted.plotOn(frame_control)
+# model_X.paramOn(frame_control, RF.Layout(0.55, 0.87, 0.6))
+# model_X.plotOn(frame_control, RF.LineColor(ROOT.kRed-6), RF.LineWidth(5))
+#
+# frame_control.Draw()
 
 ################
 ##_-_-_-_-_-_-#
@@ -190,7 +213,7 @@ frame_control.Draw()
 c.cd(4)
 
 N_sig_X.setVal(50)
-N_bkgr_control.setVal(0)
+N_bkgr_control.setVal(5)
 N_sig_X.setConstant(0)
 
 rrr_sig = model_X.fitTo(data_X_weighted, RF.Save(), RF.SumW2Error(ROOT.kFALSE))
@@ -211,10 +234,10 @@ frame_control.Draw()
 
 
 ###############################################################################################################################
-nll_sig  = rrr_sig.minNll()
-nll_null = rrr_null.minNll()
-P = ROOT.TMath.Prob(nll_null - nll_sig, 1)## !!! Change delta of ndf appropriately
-S = ROOT.TMath.ErfcInverse(P) * math.sqrt(2)
-print 'P=', P, ' nll_sig=', nll_sig, ' nll_null=', nll_null, '\n', 'S=', S
+# nll_sig  = rrr_sig.minNll()
+# nll_null = rrr_null.minNll()
+# P = ROOT.TMath.Prob(nll_null - nll_sig, 1) ## !!! Change delta of ndf appropriately
+# S = ROOT.TMath.ErfcInverse(P) * math.sqrt(2)
+# print 'P=', P, ' nll_sig=', nll_sig, ' nll_null=', nll_null, '\n', 'S=', S
 
 c.SaveAs('sPlot.png')
