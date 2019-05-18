ec58b3b76352ed63fb043c5cd772cf2fd07ecabb
Oleg Filatov <oleg.filatov@cern.ch>
2019-05-18 19:41:51 +0300

    new range Bs range and flaoting bkgr params - fits OK, pvalues as well

--- sPlot.py
+++ sPlot.py
@@ -209,9 +209,12 @@ a1_phi.setConstant(1); a2_phi.setConstant(1); a3_phi.setConstant(1); a4_phi.setC
 a1_ext.setConstant(1); a2_ext.setConstant(1); a3_ext.setConstant(1); a4_ext.setConstant(1);
 
 model[sPlot_from].fitTo(data_sig, RF.Extended(ROOT.kTRUE))
-# model[sPlot_from].fitTo(data_sig, RF.Extended(ROOT.kTRUE))
-# model[sPlot_from].fitTo(data_sig, RF.Extended(ROOT.kTRUE))
+model[sPlot_from].fitTo(data_sig, RF.Extended(ROOT.kTRUE))
+a1.setConstant(0); a2.setConstant(0); a3.setConstant(0); a4.setConstant(0);
+a1_phi.setConstant(0); a2_phi.setConstant(0); a3_phi.setConstant(0); a4_phi.setConstant(0);
+a1_ext.setConstant(0); a2_ext.setConstant(0); a3_ext.setConstant(0); a4_ext.setConstant(0);
 
+model[sPlot_from].fitTo(data_sig, RF.Extended(ROOT.kTRUE))
 
 # w = ROOT.RooWorkspace("w", True)
 # Import = getattr(ROOT.RooWorkspace, 'import')
@@ -242,15 +245,12 @@ model[sPlot_from].fitTo(data_sig, RF.Extended(ROOT.kTRUE))
 # file_out_data.write(str(N[sPlot_from].getVal()) + ' ' + str(N[sPlot_from].getError()) + '\n')
 plot_on_frame(var[sPlot_from], data_sig, model[sPlot_from], '', left[sPlot_from], right[sPlot_from], nbins[sPlot_from], None, False, chi_dict)
 # plot_pull(var[sPlot_from], data_sig, model[sPlot_from], save = True)
-plot_MCStudy(var[sPlot_from], model[sPlot_from], var_to_study = N_sig_Bs,  N_toys = 1000, N_gen = int(data_sig.sumEntries()), save = True, label = data_sig.GetName())
+# plot_MCStudy(var[sPlot_from], model[sPlot_from], var_to_study = N_sig_Bs,  N_toys = 1000, N_gen = int(data_sig.sumEntries()), save = True, label = data_sig.GetName())
 
 
 CMS_tdrStyle_lumi.CMS_lumi( c_sPlot_1, 2, 0 ); c_sPlot_1.Update(); c_sPlot_1.RedrawAxis(); # c_sPlot_1.GetFrame().Draw();
 # c_sPlot_1.SaveAs('~/Study/Bs_resonances/' + sPlot_from_text + '->' + sPlot_to_text + '/c_sPlot_1_' + str(mode) + refl_line + '.pdf')
 
-a1.setConstant(0); a2.setConstant(0); a3.setConstant(0); a4.setConstant(0);
-a1_phi.setConstant(0); a2_phi.setConstant(0); a3_phi.setConstant(0); a4_phi.setConstant(0);
-a1_ext.setConstant(0); a2_ext.setConstant(0); a3_ext.setConstant(0); a4_ext.setConstant(0);
 
 #             #--------------#
 #             ##  sPlot II  ##
