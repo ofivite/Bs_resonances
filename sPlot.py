0c8aea8af08b99a9c235534f2c60ae21c992fcdd
Oleg Filatov <oleg.filatov@cern.ch>
2019-07-26 19:29:38 +0300

    fix to display CMS style; flipping works;

--- sPlot.py
+++ sPlot.py
@@ -39,21 +39,22 @@ N_B0_refl.setVal(0.); N_B0_refl.setConstant(1)
             #---------------#
 
 DE_inclus = DataExplorer(label=REGIONS_FROM, data=data, model=model[REGIONS_FROM])
-DE_inclus.set_regions()
 if REGIONS_FROM == 'phi':
     DE_inclus.window = 0.01
     DE_inclus.distance_to_sdb = 0.005
+else:
+    DE_inclus.set_regions()
 #
 data_sig, data_side = DE_inclus.get_regions()
 fit_res_inclus = DE_inclus.fit(is_sum_w2=False)
 #
 c_inclus = ROOT.TCanvas("c_inclus", "c_inclus", 800, 600)
-CMS_tdrStyle_lumi.CMS_lumi(c_inclus, 2, 0);
 frame_inclus = DE_inclus.plot_on_frame(plot_params=plot_param[REGIONS_FROM]);
 frame_inclus_max = frame_inclus.GetMaximum(); coeffs = array([0.75, 0.85, 0.95]) if MODE == 'X' else array([0.5, 0.62, 0.75])
 y_sdb_left, y_sr, y_sdb_right = frame_inclus_max*coeffs
 frame_inclus = DE_inclus.plot_regions(frame_inclus, y_sdb_left, y_sr, y_sdb_right)
 frame_inclus.Draw()
+CMS_tdrStyle_lumi.CMS_lumi(c_inclus, 2, 0)
 #
 chi2_results.update(DE_inclus.chi2_test())
 # c_inclus.Update(); c_inclus.RedrawAxis(); #c_inclus.GetFrame().Draw();
@@ -69,14 +70,16 @@ else:        N_B0_refl.setVal(0.); N_B0_refl.setConstant(1)
 DE_1 = DataExplorer(label=SPLOT_FROM, data=data_sig, model=model[SPLOT_FROM])
 fit_res_1 = DE_1.fit(fix_float=bkgr_params[SPLOT_FROM], is_sum_w2=False)
 #
-c_sPlot_1 = ROOT.TCanvas("c_sPlot_1", "c_sPlot_1", 800, 600); CMS_tdrStyle_lumi.CMS_lumi(c_sPlot_1, 2, 0);
+c_sPlot_1 = ROOT.TCanvas("c_sPlot_1", "c_sPlot_1", 800, 600)
 frame_DE_1 = DE_1.plot_on_frame(plot_params=plot_param[SPLOT_FROM])
 frame_DE_1.Draw()
+CMS_tdrStyle_lumi.CMS_lumi(c_sPlot_1, 2, 0)
 #
 chi2_results.update(DE_1.chi2_test())
+# #
 # w1 = DE_1.prepare_workspace(poi = N_sig[SPLOT_FROM], nuisances=bkgr_params[SPLOT_FROM] + [mean[SPLOT_FROM], N_bkgr[SPLOT_FROM]])
 # DE_1.toy_signif(w=w1, n_toys = 10, seed=333)
-
+# #
 # c_sPlot_1.Update(); c_sPlot_1.RedrawAxis(); # c_sPlot_1.GetFrame().Draw();
 # c_sPlot_1.SaveAs('~/Study/Bs_resonances/' + SPLOT_FROM + '->' + SPLOT_TO + '/c_sPlot_1_' + MODE + refl_line + '.pdf')
 # # file_out_data.write(str(N_sig[SPLOT_FROM].getVal()) + ' ' + str(N_sig[SPLOT_FROM].getError()) + '\n')
@@ -91,13 +94,14 @@ data_sig_w = ROOT.RooDataSet(data_sig.GetName(), data_sig.GetTitle(), data_sig, 
 data_sig_w.SetName('sig_w')
 hist_sig_weighted = ROOT.RooDataHist('hist_sig_weighted', 'hist_sig_weighted', ROOT.RooArgSet(var[SPLOT_TO]), data_sig_w) ### binning for this var was already previously set
 #
-mean_MC_val = w[SPLOT_TO].var(mean[SPLOT_TO].GetName()).getVal(); mean[SPLOT_TO].setVal(mean_MC_val); mean[SPLOT_TO].setConstant(1)
 DE_2 = DataExplorer(label=SPLOT_TO, data=data_sig_w, model=model[SPLOT_TO])
-fit_res_2 = DE_2.fit(is_sum_w2=True)
+#mean[SPLOT_TO].setConstant(1)
+fit_res_2 = DE_2.fit(is_sum_w2=True, fix_float=bkgr_params[SPLOT_TO])
 #
-c_sPlot_2 = ROOT.TCanvas("c_sPlot_2", "c_sPlot_2", 800, 600); CMS_tdrStyle_lumi.CMS_lumi(c_sPlot_2, 2, 0);
+c_sPlot_2 = ROOT.TCanvas("c_sPlot_2", "c_sPlot_2", 800, 600)
 frame_DE_2 = DE_2.plot_on_frame(plot_params=plot_param[SPLOT_TO])
 frame_DE_2.Draw()
+CMS_tdrStyle_lumi.CMS_lumi(c_sPlot_2, 2, 0)
 #
 chi2_results.update(DE_2.chi2_test())
 # c_sPlot_2.Update(); c_sPlot_2.RedrawAxis(); # c_sPlot_2.GetFrame().Draw();
@@ -112,15 +116,16 @@ mean[SPLOT_FROM].setConstant(1)
 N_B0_refl.setVal(0.); N_B0_refl.setConstant(1)
 #
 DE_3 = DataExplorer(label=SPLOT_FROM, data=data_side, model=model[SPLOT_FROM])
-fit_res_3 = DE_3.fit(fix_float=bkgr_params[SPLOT_FROM], is_sum_w2=False)
+fit_res_3 = DE_3.fit(is_sum_w2=False, fix_float=bkgr_params[SPLOT_FROM])
 #
 # w3 = DE_3.prepare_workspace(poi=N_sig[SPLOT_FROM], nuisances=bkgr_params[SPLOT_FROM] + [mean[SPLOT_FROM], N_bkgr[SPLOT_FROM]])
 # asympt_rrr = DE_3.asympt_signif(w=w3)
 # asympt_rrr.Print()
 #
-c_sPlot_3 = ROOT.TCanvas("c_sPlot_3", "c_sPlot_3", 800, 600); CMS_tdrStyle_lumi.CMS_lumi(c_sPlot_3, 2, 0);
+c_sPlot_3 = ROOT.TCanvas("c_sPlot_3", "c_sPlot_3", 800, 600)
 frame_DE_3 = DE_3.plot_on_frame()
 frame_DE_3.Draw()
+CMS_tdrStyle_lumi.CMS_lumi(c_sPlot_3, 2, 0)
 #
 chi2_results.update(DE_3.chi2_test())
 # c_sPlot_3.Update(); c_sPlot_3.RedrawAxis(); # c_sPlot_3.GetFrame().Draw();
@@ -139,15 +144,16 @@ data_side_w.SetName('side_w')
 hist_side_weighted = ROOT.RooDataHist('hist_side_weighted', 'hist_side_weighted', ROOT.RooArgSet(var[SPLOT_TO]), data_side_w) ### binning for this var was already previously set
 #
 DE_4 = DataExplorer(label=SPLOT_TO, data=hist_side_weighted, model=model[SPLOT_TO])
-DE_4.chi2_fit()
+DE_4.fit(is_sum_w2=True, fix_float=bkgr_params[SPLOT_TO])
 # #
 # w4 = DE_4.prepare_workspace(poi = fr_model_phi, nuisances = bkgr_params[SPLOT_TO] + [mean[SPLOT_TO]])
 # asympt_rrr = DE_4.asympt_signif(w = w4)
 # asympt_rrr.Print()
 # #
-c_sPlot_4 = ROOT.TCanvas("c_sPlot_4", "c_sPlot_4", 800, 600); CMS_tdrStyle_lumi.CMS_lumi(c_sPlot_4, 2, 0);
+c_sPlot_4 = ROOT.TCanvas("c_sPlot_4", "c_sPlot_4", 800, 600)
 frame_DE_4 = DE_4.plot_on_frame()
 frame_DE_4.Draw()
+CMS_tdrStyle_lumi.CMS_lumi(c_sPlot_4, 2, 0)
 #
 chi2_results.update(DE_4.chi2_test())
 # c_sPlot_4.Update(); c_sPlot_4.RedrawAxis(); # c_sPlot_4.GetFrame().Draw();
